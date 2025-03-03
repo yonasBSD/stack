3e381bdaee9c9c4fe2a112376c10d7c893207118
Konstantin Wohlwend <n2d4xc@gmail.com>
2025-03-02 17:31:33 -0800

    Fix tests

--- apps/backend/src/prisma-client.tsx
+++ apps/backend/src/prisma-client.tsx
@@ -27,30 +27,39 @@ export async function retryTransaction<T>(fn: (...args: Parameters<Parameters<ty
 
   return await traceSpan('Prisma transaction', async (span) => {
     const res = await Result.retry(async (attemptIndex) => {
-      return await traceSpan(`transaction attempt #${attemptIndex}`, async () => {
-        try {
-          return await prismaClient.$transaction(async (...args) => {
-            try {
-              return Result.ok(await fn(...args));
-            } catch (e) {
-              if (e instanceof Prisma.PrismaClientKnownRequestError || e instanceof Prisma.PrismaClientUnknownRequestError) {
-                // retry
-                return Result.error(e);
+      return await traceSpan(`transaction attempt #${attemptIndex}`, async (attemptSpan) => {
+        const attemptRes = await (async () => {
+          try {
+            return await prismaClient.$transaction(async (...args) => {
+              try {
+                return Result.ok(await fn(...args));
+              } catch (e) {
+                if (e instanceof Prisma.PrismaClientKnownRequestError || e instanceof Prisma.PrismaClientUnknownRequestError) {
+                  // retry
+                  return Result.error(e);
+                }
+                throw e;
               }
-              throw e;
+            }, {
+              isolationLevel: enableSerializable && attemptIndex < 2 ? Prisma.TransactionIsolationLevel.Serializable : undefined,
+            });
+          } catch (e) {
+            // we don't want to retry as aggressively here, because the error may have been thrown after the transaction was already committed
+            // so, we select the specific errors that we know are safe to retry
+            if ([
+              "Transaction failed due to a write conflict or a deadlock. Please retry your transaction",
+              "Transaction already closed: A commit cannot be executed on an expired transaction. The timeout for this transaction",
+            ].some(s => e instanceof Prisma.PrismaClientKnownRequestError && e.message.includes(s))) {
+              // transaction timeout, retry
+              return Result.error(e);
             }
-          }, {
-            isolationLevel: enableSerializable && attemptIndex < 2 ? Prisma.TransactionIsolationLevel.Serializable : undefined,
-          });
-        } catch (e) {
-          // we don't want to retry as aggressively here, because the error may have been thrown after the transaction was already committed
-          // so, we select the specific errors that we know are safe to retry
-          if (e instanceof Prisma.PrismaClientKnownRequestError && e.message.includes("Transaction already closed: A commit cannot be executed on an expired transaction. The timeout for this transaction")) {
-            // transaction timeout, retry
-            return Result.error(e);
+            throw e;
           }
-          throw e;
+        })();
+        if (attemptRes.status === "error") {
+          attemptSpan.setAttribute("stack.prisma.transaction-retry.error", `${attemptRes.error}`);
         }
+        return attemptRes;
       });
     }, isDev ? 1 : 3);
 
