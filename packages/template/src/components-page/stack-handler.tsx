8d6c6e314bca7e3bc81637f341ec0904241829b5
Konsti Wohlwend <n2d4xc@gmail.com>
2025-04-25 17:22:23 -0700

    Revert "Fix/disable forgot password when auth disabled" (#649)
    Reverts stack-auth/stack-auth#562

--- packages/template/src/components-page/stack-handler.tsx
+++ packages/template/src/components-page/stack-handler.tsx
@@ -77,15 +77,11 @@ function renderComponent(props: {
   redirectIfNotHandler?: (name: keyof HandlerUrls) => void,
   onNotFound: () => any,
   app: StackClientApp<any> | StackServerApp<any>,
-  user: any,
 }) {
-  const { path, searchParams, fullPage, componentProps, redirectIfNotHandler, onNotFound, app, user } = props;
+  const { path, searchParams, fullPage, componentProps, redirectIfNotHandler, onNotFound, app } = props;
 
   switch (path) {
     case availablePaths.signIn: {
-      if (user) {
-        redirect(app.urls.afterSignIn);
-      }
       redirectIfNotHandler?.('signIn');
       return <SignIn
         fullPage={fullPage}
@@ -94,9 +90,6 @@ function renderComponent(props: {
       />;
     }
     case availablePaths.signUp: {
-      if (user) {
-        redirect(app.urls.afterSignUp);
-      }
       redirectIfNotHandler?.('signUp');
       return <SignUp
         fullPage={fullPage}
@@ -113,9 +106,6 @@ function renderComponent(props: {
       />;
     }
     case availablePaths.passwordReset: {
-      if (user) {
-        redirect(app.urls.afterSignIn);
-      }
       redirectIfNotHandler?.('passwordReset');
       return <PasswordReset
         searchParams={searchParams}
@@ -124,9 +114,6 @@ function renderComponent(props: {
       />;
     }
     case availablePaths.forgotPassword: {
-      if (user) {
-        redirect(app.urls.afterSignIn);
-      }
       redirectIfNotHandler?.('forgotPassword');
       return <ForgotPassword
         fullPage={fullPage}
@@ -134,9 +121,6 @@ function renderComponent(props: {
       />;
     }
     case availablePaths.signOut: {
-      if (!user) {
-        redirect(app.urls.signIn);
-      }
       redirectIfNotHandler?.('signOut');
       return <SignOut
         fullPage={fullPage}
@@ -144,9 +128,6 @@ function renderComponent(props: {
       />;
     }
     case availablePaths.oauthCallback: {
-      if (user) {
-        redirect(app.urls.afterSignIn);
-      }
       redirectIfNotHandler?.('oauthCallback');
       return <OAuthCallback
         fullPage={fullPage}
@@ -154,9 +135,6 @@ function renderComponent(props: {
       />;
     }
     case availablePaths.magicLinkCallback: {
-      if (user) {
-        redirect(app.urls.afterSignIn);
-      }
       redirectIfNotHandler?.('magicLinkCallback');
       return <MagicLinkCallback
         searchParams={searchParams}
@@ -173,17 +151,12 @@ function renderComponent(props: {
       />;
     }
     case availablePaths.accountSettings: {
-      if (!user) {
-        redirect(app.urls.signIn);
-      }
-      redirectIfNotHandler?.('accountSettings');
       return <AccountSettings
         fullPage={fullPage}
         {...filterUndefinedINU(componentProps?.AccountSettings)}
       />;
     }
     case availablePaths.error: {
-      redirectIfNotHandler?.('error');
       return <ErrorPage
         searchParams={searchParams}
         fullPage={fullPage}
@@ -227,7 +200,7 @@ async function NextStackHandler<HasTokenStore extends boolean>(props: BaseHandle
   const routeProps = "routeProps" in props ? props.routeProps as RouteProps : pick(props, ["params", "searchParams"] as any);
   const params = await routeProps.params;
   const searchParams = await routeProps.searchParams;
-  const user = await props.app.getUser();
+
   if (!params?.stack) {
     return (
       <MessageCard title="Invalid Stack Handler Setup" fullPage={props.fullPage}>
@@ -264,7 +237,6 @@ async function NextStackHandler<HasTokenStore extends boolean>(props: BaseHandle
     redirectIfNotHandler,
     onNotFound: () => notFound(),
     app: props.app,
-    user,
   });
 
   if (result && 'redirect' in result) {
@@ -305,8 +277,6 @@ function ReactStackHandler<HasTokenStore extends boolean>(props: BaseHandlerProp
     };
   }, [props.location, props.app.urls.handler]);
 
-  const user = props.app.useUser();
-
   const redirectIfNotHandler = (name: keyof HandlerUrls) => {
     const url = props.app.urls[name];
     const handlerUrl = props.app.urls.handler;
@@ -340,7 +310,6 @@ function ReactStackHandler<HasTokenStore extends boolean>(props: BaseHandlerProp
       </MessageCard>
     ),
     app: props.app,
-    user,
   });
 
   if (result && 'redirect' in result) {
