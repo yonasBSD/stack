335bc97027ce85331d44e30baad5313461ac58ac
Konstantin Wohlwend <n2d4xc@gmail.com>
2025-04-30 11:11:54 -0700

    Wrap migration into small transactions

--- apps/backend/prisma/migrations/20250425171311_remove_old_config/migration.sql
+++ apps/backend/prisma/migrations/20250425171311_remove_old_config/migration.sql
@@ -167,14 +167,17 @@ ALTER TABLE "AuthMethod" DROP COLUMN "authMethodConfigId",
 DROP COLUMN "projectConfigId";
 
 -- AlterTable
+BEGIN;
 ALTER TABLE "ConnectedAccount" ADD COLUMN "configOAuthProviderId" TEXT;
 UPDATE "ConnectedAccount" SET "configOAuthProviderId" = "oauthProviderConfigId";
 ALTER TABLE "ConnectedAccount" ALTER COLUMN "configOAuthProviderId" SET NOT NULL;
 ALTER TABLE "ConnectedAccount" DROP COLUMN "oauthProviderConfigId";
 ALTER TABLE "ConnectedAccount" DROP COLUMN "connectedAccountConfigId";
 ALTER TABLE "ConnectedAccount" DROP COLUMN "projectConfigId";
+COMMIT;
 
 -- AlterTable
+BEGIN;
 ALTER TABLE "EmailTemplate" DROP CONSTRAINT "EmailTemplate_pkey";
 ALTER TABLE "EmailTemplate" ADD COLUMN "projectId" TEXT;
 
@@ -201,30 +204,38 @@ ALTER TABLE "EmailTemplate" ADD CONSTRAINT "EmailTemplate_pkey" PRIMARY KEY ("pr
 
 -- Drop the old column
 ALTER TABLE "EmailTemplate" DROP COLUMN "projectConfigId";
+COMMIT;
 
 -- AlterTable
+BEGIN;
 ALTER TABLE "OAuthAccessToken" ADD COLUMN "configOAuthProviderId" TEXT;
 UPDATE "OAuthAccessToken" SET "configOAuthProviderId" = "oAuthProviderConfigId";
 ALTER TABLE "OAuthAccessToken" ALTER COLUMN "configOAuthProviderId" SET NOT NULL;
 ALTER TABLE "OAuthAccessToken" DROP COLUMN "oAuthProviderConfigId";
+COMMIT;
 
 -- AlterTable
+BEGIN;
 ALTER TABLE "OAuthAuthMethod" ADD COLUMN "configOAuthProviderId" TEXT;
 UPDATE "OAuthAuthMethod" SET "configOAuthProviderId" = "oauthProviderConfigId";
 ALTER TABLE "OAuthAuthMethod" ALTER COLUMN "configOAuthProviderId" SET NOT NULL;
 ALTER TABLE "OAuthAuthMethod" DROP COLUMN "oauthProviderConfigId";
 ALTER TABLE "OAuthAuthMethod" DROP COLUMN "projectConfigId";
+COMMIT;
 
 -- AlterTable
+BEGIN;
 ALTER TABLE "OAuthToken" ADD COLUMN "configOAuthProviderId" TEXT;
 UPDATE "OAuthToken" SET "configOAuthProviderId" = "oAuthProviderConfigId";
 ALTER TABLE "OAuthToken" ALTER COLUMN "configOAuthProviderId" SET NOT NULL;
 ALTER TABLE "OAuthToken" DROP COLUMN "oAuthProviderConfigId";
+COMMIT;
 
 -- AlterTable
 ALTER TABLE "Project" DROP COLUMN "configId";
 
 -- AlterTable
+BEGIN;
 ALTER TABLE "ProjectUserDirectPermission" ADD COLUMN "permissionId" TEXT;
 
 -- Update permissionId with values from Permission table
@@ -238,8 +249,10 @@ ALTER TABLE "ProjectUserDirectPermission" ALTER COLUMN "permissionId" SET NOT NU
 
 -- Drop the old column
 ALTER TABLE "ProjectUserDirectPermission" DROP COLUMN "permissionDbId";
+COMMIT;
 
 -- AlterTable
+BEGIN;
 ALTER TABLE "ProjectUserOAuthAccount" ADD COLUMN "configOAuthProviderId" TEXT;
 UPDATE "ProjectUserOAuthAccount" SET "configOAuthProviderId" = "oauthProviderConfigId";
 ALTER TABLE "ProjectUserOAuthAccount" ALTER COLUMN "configOAuthProviderId" SET NOT NULL;
@@ -247,8 +260,10 @@ ALTER TABLE "ProjectUserOAuthAccount" DROP CONSTRAINT "ProjectUserOAuthAccount_p
 ALTER TABLE "ProjectUserOAuthAccount" DROP COLUMN "oauthProviderConfigId";
 ALTER TABLE "ProjectUserOAuthAccount" DROP COLUMN "projectConfigId";
 ALTER TABLE "ProjectUserOAuthAccount" ADD CONSTRAINT "ProjectUserOAuthAccount_pkey" PRIMARY KEY ("tenancyId", "configOAuthProviderId", "providerAccountId");
+COMMIT;
 
 -- AlterTable
+BEGIN;
 ALTER TABLE "TeamMemberDirectPermission" ADD COLUMN "permissionId" TEXT;
 
 -- Check for rows where both or neither field is populated
@@ -280,6 +295,7 @@ ALTER TABLE "TeamMemberDirectPermission" ALTER COLUMN "permissionId" SET NOT NUL
 -- Then drop the old columns
 ALTER TABLE "TeamMemberDirectPermission" DROP COLUMN "permissionDbId";
 ALTER TABLE "TeamMemberDirectPermission" DROP COLUMN "systemPermission";
+COMMIT;
 
 -- DropTable
 DROP TABLE "AuthMethodConfig";
