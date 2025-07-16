8c48d7258ba6f90bd28f0f7d5ad72c5f9dd4dacd
Konstantin Wohlwend <n2d4xc@gmail.com>
2025-07-16 12:05:08 -0700

    Next.js 15.4

--- apps/e2e/tests/backend/endpoints/api/v1/users.test.ts
+++ apps/e2e/tests/backend/endpoints/api/v1/users.test.ts
@@ -1382,10 +1382,10 @@ describe("with server access", () => {
         "status": 409,
         "body": {
           "code": "USER_EMAIL_ALREADY_EXISTS",
-        "details": {
-          "email": "default-mailbox--<stripped UUID>@stack-generated.example.com",
-          "would_work_if_email_was_verified": false,
-        },
+          "details": {
+            "email": "default-mailbox--<stripped UUID>@stack-generated.example.com",
+            "would_work_if_email_was_verified": false,
+          },
           "error": "A user with email \\"default-mailbox--<stripped UUID>@stack-generated.example.com\\" already exists.",
         },
         "headers": Headers {
@@ -1947,10 +1947,10 @@ describe("with server access", () => {
         "status": 409,
         "body": {
           "code": "USER_EMAIL_ALREADY_EXISTS",
-        "details": {
-          "email": "default-mailbox--<stripped UUID>@stack-generated.example.com",
-          "would_work_if_email_was_verified": false,
-        },
+          "details": {
+            "email": "default-mailbox--<stripped UUID>@stack-generated.example.com",
+            "would_work_if_email_was_verified": false,
+          },
           "error": "A user with email \\"default-mailbox--<stripped UUID>@stack-generated.example.com\\" already exists.",
         },
         "headers": Headers {
@@ -2010,10 +2010,10 @@ describe("with server access", () => {
         "status": 409,
         "body": {
           "code": "USER_EMAIL_ALREADY_EXISTS",
-        "details": {
-          "email": "default-mailbox--<stripped UUID>@stack-generated.example.com",
-          "would_work_if_email_was_verified": false,
-        },
+          "details": {
+            "email": "default-mailbox--<stripped UUID>@stack-generated.example.com",
+            "would_work_if_email_was_verified": false,
+          },
           "error": "A user with email \\"default-mailbox--<stripped UUID>@stack-generated.example.com\\" already exists.",
         },
         "headers": Headers {
