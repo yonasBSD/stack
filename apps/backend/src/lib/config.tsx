82115700f2308e02ca660201cfc02695f8f99c74
Konstantin Wohlwend <n2d4xc@gmail.com>
2025-07-28 15:23:09 -0700

    Fix tests

--- apps/backend/src/lib/config.tsx
+++ apps/backend/src/lib/config.tsx
@@ -114,9 +114,9 @@ export function getProjectConfigOverrideQuery(options: ProjectOptions): RawQuery
         throw new StackAssertionError(`Expected 0 or 1 project config overrides for project ${options.projectId}, got ${queryResult.length}`, { queryResult });
       }
       if (queryResult.length === 0) {
-        return {};
+        throw new StackAssertionError(`Expected 1 project config override for project ${options.projectId}, got 0`, { queryResult, options });
       }
-      return queryResult[0].projectConfigOverride;
+      return queryResult[0].projectConfigOverride ?? {};
     },
   };
 }
