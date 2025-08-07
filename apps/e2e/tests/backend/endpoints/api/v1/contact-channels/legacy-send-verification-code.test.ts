98d6ca3601b46db17ea3c8f55440a7ec5a894890
Konstantin Wohlwend <n2d4xc@gmail.com>
2025-08-07 10:09:32 -0700

    Even more resilient tests

--- apps/e2e/tests/backend/endpoints/api/v1/contact-channels/legacy-send-verification-code.test.ts
+++ apps/e2e/tests/backend/endpoints/api/v1/contact-channels/legacy-send-verification-code.test.ts
@@ -1,3 +1,4 @@
+import { wait } from "@stackframe/stack-shared/dist/utils/promises";
 import { it } from "../../../../../helpers";
 import { Auth, backendContext, niceBackendFetch } from "../../../../backend-helpers";
 
@@ -48,6 +49,7 @@ it("should send a verification code per e-mail", async ({ expect }) => {
       callback_url: "http://localhost:12345/some-callback-url",
     },
   });
+  await wait(1000);
   expect(await backendContext.value.mailbox.fetchMessages({ noBody: true })).toMatchInlineSnapshot(`
     [
       MailboxMessage {
