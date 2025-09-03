faf79e5a9e5fa8ad608e7d5634339743a55d06f0
Konstantin Wohlwend <n2d4xc@gmail.com>
2025-09-03 10:44:52 -0700

    Speed up Vitest

--- vitest.shared.ts
+++ vitest.shared.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   plugins: [tsconfigPaths() as any],
   test: {
+    pool: 'threads',
+    maxWorkers: 8,
     include: ['**/*.test.{js,ts,jsx,tsx}'],
     includeSource: ['**/*.{js,ts,jsx,tsx}'],
   },
