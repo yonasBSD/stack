d5c815f42b5861075b0506b787a14582561d60c5
Konstantin Wohlwend <n2d4xc@gmail.com>
2025-11-20 17:57:41 -0800

    Instructions on how to sign in to internal account during development

--- apps/dashboard/src/app/(main)/handler/[...stack]/page.tsx
+++ apps/dashboard/src/app/(main)/handler/[...stack]/page.tsx
@@ -1,11 +1,18 @@
-import { StackHandler } from "@stackframe/stack";
-import { stackServerApp } from "@/stack";
 import { StyledLink } from "@/components/link";
+import { stackServerApp } from "@/stack";
+import { StackHandler } from "@stackframe/stack";
 
 export default function Handler(props: unknown) {
   const extraInfo = <>
     <p className="text-xs">By signing in, you agree to the</p>
     <p className="text-xs"><StyledLink href="https://www.iubenda.com/privacy-policy/19290387/cookie-policy">Terms of Service</StyledLink> and <StyledLink href="https://www.iubenda.com/privacy-policy/19290387">Privacy Policy</StyledLink></p>
+    {process.env.NODE_ENV === "development" ?
+      <div className="relative">
+        <div className="bg-red-500 text-white p-2 rounded-md m-2 animate-bounce [animation-duration:2s]">
+          Looks like you&apos;re in development mode! Sign in with GitHub and then admin@example.com to access the admin user with the internal & the dummy project.
+        </div>
+      </div>
+      : null}
   </>;
   return <StackHandler
     fullPage
